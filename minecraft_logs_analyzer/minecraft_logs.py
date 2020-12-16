1699f232a7eafef9737f7846e5e0916d0e6b2638
Hawkpath <hawkpathas@gmail.com>
2020-12-16 18:59:00 -0500

    Fix weird yield from context manager. This behavior is not well-defined. Instead, yield the opened file and manage with try-finally.

--- minecraft_logs_analyzer/minecraft_logs.py
+++ minecraft_logs_analyzer/minecraft_logs.py
@@ -5,7 +5,7 @@ import gzip
 import os
 import re
 from pathlib import Path
-from typing import Pattern
+from typing import *
 
 
 def read_backward_until(stream, delimiter, buf_size=32, stop_after=1,
@@ -73,7 +73,7 @@ def read_last_line(stream):
     return read_backward_until(stream, os.linesep, stop_after=2, trim_start=2)
 
 
-def iter_logs(path):
+def iter_logs(path: Union[str, Path]) -> Generator[TextIO]:
     if isinstance(path, str):
         path = Path(path)
     elif not isinstance(path, Path):
@@ -86,9 +86,9 @@ def iter_logs(path):
             continue
         elif not file.name.startswith('20'):
             continue
-        with open_methods[file.suffix](file, 'rt', encoding='ansi',
-                                       errors='replace', newline='') as f:
-            yield f
+        yield open_methods[file.suffix](
+            file, 'rt', encoding='ansi', errors='replace', newline=''
+        )
 
 
 def count_playtime(path, count=-1, print_files='file'):
@@ -102,64 +102,68 @@ def count_playtime(path, count=-1, print_files='file'):
     total_time = timedelta()
 
     for log in iter_logs(path):
-        if stop_scan:
-            stop_scan = False
-            insert("\nTotal Time:" + " " + str(total_time))
-            data_total_play_time = total_time
-            return
-        if count == 0:
-            return
-        count -= 1
-
         try:
-            start_time = time_pattern.match(log.readline()).groupdict()
-            end_time = time_pattern.match(
-                read_backward_until(log, time_pattern_simple)).groupdict()
-        except AttributeError as e:
-            # Not a recognized chat log
-            insert("ERROR: {} generated this error: {}".format(Path(log.name).name,e))
-            continue
-        except EOFError:
-            insert('ERROR: {} may be corrupted -- skipping'.format(Path(log.name).name))
-            continue
-        except OSError:
-            insert('ERROR: {} may be corrupted or is not gzipped -- skipping'.format(Path(log.name).name))
-            continue
-        except:
-            insert('ERROR: An error occured while scanning file {} -- skiping')
-        start_time = timedelta(
-            hours=int(start_time['hour']),
-            minutes=int(start_time['min']),
-            seconds=int(start_time['sec'])
-        )
-        end_time = timedelta(
-            hours=int(end_time['hour']),
-            minutes=int(end_time['min']),
-            seconds=int(end_time['sec'])
-        )
-        if end_time < start_time:
-            end_time += timedelta(days=1)
-        delta = end_time - start_time
-        total_time += delta
-        if print_files == 'full':
-            insert(str(log.name)+" "+str(delta))
-        elif print_files == 'file':
-            insert(str(Path(log.name).name)+" "+str(delta))
-        # collect data for csv
-        csv_data[str(Path(log.name).name)[:12]] = str(delta)
+            if stop_scan:
+                stop_scan = False
+                insert("\nTotal Time:" + " " + str(total_time))
+                data_total_play_time = total_time
+                return
+            if count == 0:
+                return
+            count -= 1
 
-        # collect data for graph
-        month = str(Path(log.name).name)[:7]
-        if current_month != month:  # Check if we are still on the same month if not save the current month and move on
-            if current_month != '':
-                if current_month not in graph_data_collection:
-                    graph_data_collection[current_month] = 0
-                graph_data_collection[current_month] += int(total_data_time/3600) # make seconds an hour this will mean that if you played less then an hour it will end up as 0
-            # add first month and next
-            current_month = month
-            total_data_time = delta.total_seconds()
-        else:
-            total_data_time += delta.total_seconds()
-            data_total_play_time = total_time
+            try:
+                start_time = time_pattern.match(log.readline()).groupdict()
+                end_time = time_pattern.match(
+                    read_backward_until(log, time_pattern_simple)).groupdict()
+            except AttributeError as e:
+                # Not a recognized chat log
+                insert("ERROR: {} generated this error: {}".format(Path(log.name).name,e))
+                continue
+            except EOFError:
+                insert('ERROR: {} may be corrupted -- skipping'.format(Path(log.name).name))
+                continue
+            except OSError:
+                insert('ERROR: {} may be corrupted or is not gzipped -- skipping'.format(Path(log.name).name))
+                continue
+            except:
+                insert('ERROR: An error occured while scanning file {} -- skiping')
+            start_time = timedelta(
+                hours=int(start_time['hour']),
+                minutes=int(start_time['min']),
+                seconds=int(start_time['sec'])
+            )
+            end_time = timedelta(
+                hours=int(end_time['hour']),
+                minutes=int(end_time['min']),
+                seconds=int(end_time['sec'])
+            )
+            if end_time < start_time:
+                end_time += timedelta(days=1)
+            delta = end_time - start_time
+            total_time += delta
+            if print_files == 'full':
+                insert(str(log.name)+" "+str(delta))
+            elif print_files == 'file':
+                insert(str(Path(log.name).name)+" "+str(delta))
+            # collect data for csv
+            csv_data[str(Path(log.name).name)[:12]] = str(delta)
+
+            # collect data for graph
+            month = str(Path(log.name).name)[:7]
+            if current_month != month:  # Check if we are still on the same month if not save the current month and move on
+                if current_month != '':
+                    if current_month not in graph_data_collection:
+                        graph_data_collection[current_month] = 0
+                    graph_data_collection[current_month] += int(total_data_time/3600) # make seconds an hour this will mean that if you played less then an hour it will end up as 0
+                # add first month and next
+                current_month = month
+                total_data_time = delta.total_seconds()
+            else:
+                total_data_time += delta.total_seconds()
+                data_total_play_time = total_time
+
+        finally:
+            log.close()
 
     return total_time
