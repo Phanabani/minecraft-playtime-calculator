9148455e7e238abc303a97a9352be68f414ff6ef
Hawkpath <hawkpathas@gmail.com>
2020-12-22 02:28:47 -0500

    Use bash source to find the requirements file relative to the script. Is there a better way?

--- setup_linux.sh
+++ setup_linux.sh
@@ -7,6 +7,6 @@ else
   python3 -m venv venv
   source venv/bin/activate
   echo "Installing dependencies"
-  python3 -m pip install -r requirements.txt
+  python3 -m pip install -r "${BASH_SOURCE[0]}/requirements.txt"
   echo "Setup complete!"
 fi
