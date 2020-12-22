9a625af91362bc4b32d35ab642661615deb5bd97
Hawkpath <hawkpathas@gmail.com>
2020-12-22 02:03:02 -0500

    Change python to python3. I think python3 is a common binary name on Unix systems, but I might have to add a version check... This was a problem because Mac has python 2.7 built-in.

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,9 +4,9 @@ if [[ -d venv ]]; then
 else
   echo "Beginning setup"
   echo "Creating virtual environment from system Python"
-  python -m venv venv
+  python3 -m venv venv
   source venv/bin/activate
   echo "Installing dependencies"
-  python -m pip install -r requirements.txt
+  python3 -m pip install -r requirements.txt
   echo "Setup complete!"
 fi
