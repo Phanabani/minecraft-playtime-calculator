9736bf641e5ded6806c12808daef58d8e8305592
Hawkpath <hawkpathas@gmail.com>
2020-12-22 02:34:43 -0500

    Use bash source to find main.py.

--- run_linux.sh
+++ run_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -e venv/bin/activate ]]; then
   source venv/bin/activate
-  python3 main.py
+  python3 "${BASH_SOURCE[0]}/main.py"
 else
   echo "You haven't installed the virtual environment yet. Run setup.sh"
 fi
